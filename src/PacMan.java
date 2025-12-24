1e276db32b2450bf7c0a5184a7d7725373540bb5
Ahmed-Hajhssan <151759799+Ahmed-Hajhssan@users.noreply.github.com>
2025-12-24 22:15:07 +0100

    Add arrow key controls
    Add arrow key controls

--- src/PacMan.java
+++ src/PacMan.java
@@ -51,6 +51,81 @@ public class PacMan extends JPanel {
             r2.grow(-3, -3);
             return r1.intersects(r2);
         }
+
+        private void setupKeyBindings() {
+            InputMap im = getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+            ActionMap am = getActionMap();
+
+            im.put(KeyStroke.getKeyStroke("released UP"), "up");
+            am.put("up", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (gameState == GameState.PLAYING && pacman != null) {
+                        pacman.setDirection('U');
+                        pacman.setImage(pacmanUpImage);
+                    }
+                }
+            });
+
+            im.put(KeyStroke.getKeyStroke("released DOWN"), "down");
+            am.put("down", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (gameState == GameState.PLAYING && pacman != null) {
+                        pacman.setDirection('D');
+                        pacman.setImage(pacmanDownImage);
+                    }
+                }
+            });
+
+            im.put(KeyStroke.getKeyStroke("released LEFT"), "left");
+            am.put("left", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (gameState == GameState.PLAYING && pacman != null) {
+                        pacman.setDirection('L');
+                        pacman.setImage(pacmanLeftImage);
+                    }
+                }
+            });
+
+            im.put(KeyStroke.getKeyStroke("released RIGHT"), "right");
+            am.put("right", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (gameState == GameState.PLAYING && pacman != null) {
+                        pacman.setDirection('R');
+                        pacman.setImage(pacmanRightImage);
+                    }
+                }
+            });
+
+            im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false), "esc");
+            am.put("esc", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (gameState == GameState.PLAYING)
+                        pauseGame();
+                    else if (gameState == GameState.PAUSED)
+                        resumeGame();
+                    else if (gameState == GameState.MENU)
+                        System.exit(0);
+                    else if (gameState == GameState.GAMEOVER)
+                        goToMenu();
+                }
+            });
+
+            im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, false), "enter");
+            am.put("enter", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (gameState == GameState.MENU || gameState == GameState.GAMEOVER)
+                        startNewGame();
+                    else if (gameState == GameState.PAUSED)
+                        resumeGame();
+                }
+            });
+        }
 }
 
 private PacmanBlock pacman;
