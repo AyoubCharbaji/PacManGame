adaa23444cf51e958c59cbe0e9e87cdc57d7211e
Ahmed-Hajhssan <151759799+Ahmed-Hajhssan@users.noreply.github.com>
2025-12-24 14:29:05 +0100

    Implement pacman movement
    Implement pacman movement

--- src/PacMan.java
+++ src/PacMan.java
@@ -4,6 +4,10 @@ import java.awt.*;
 import javax.swing.*;
 
 public class PacMan extends JPanel {
+
+    private static final int TILE_SIZE = 32;
+    private static final int SPEED = TILE_SIZE / 6;
+
     private class PacmanBlock {
         Image image;
         int x, y, width, height;
@@ -27,6 +31,25 @@ public class PacMan extends JPanel {
 
         Rectangle getBounds() {
             return new Rectangle(x, y, width, height);
+        }
+
+        void move() {
+            int px = x, py = y;
+            if (direction == 'U')
+                y -= SPEED;
+            else if (direction == 'D')
+                y += SPEED;
+            else if (direction == 'L')
+                x -= SPEED;
+            else if (direction == 'R')
+                x += SPEED;
+        }
+
+        private boolean softCollision(Block a, Block b) {
+            Rectangle r1 = a.getBounds();
+            Rectangle r2 = b.getBounds();
+            r2.grow(-3, -3);
+            return r1.intersects(r2);
         }
 }
 
